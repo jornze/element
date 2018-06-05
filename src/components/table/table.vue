908c711fecb6550688cacfa0939d72c07a7fc206
wangziqiang <740990616@qq.com>
2018-06-05 17:04:36 +0800

    65

--- src/components/table/table.vue
+++ src/components/table/table.vue
@@ -1,83 +1,88 @@
 <template>
-<transition>
-	<div>
-		<div style="padding:20px;" id="app">
-        <div class="panel panel-primary">
-            <div class="panel-heading">用户管理</div>
-            <table class="table table-bordered table-striped text-center">
+   <transition>
+        <div>
+            <p>用户管理</p>
+            <table>
                 <thead>
-                    <tr>
-                        <th>用户名</th>
-                        <th>年龄</th>
-                        <th>毕业学校</th>
-                        <th>备注</th>
-                        <th>操作</th>
-                    </tr>
+                    <th>
+                        <td>用户名</td>
+                        <td>年龄</td>
+                        <td>毕业学校</td>
+                        <td>备注</td>
+                        <td>操作</td>
+                    </th>
                 </thead>
                 <tbody>
-                    <template v-for="row in rows ">
-                        <tr><td>{{row.Name}}</td><td>{{row.Age}}</td><td>{{row.School}}</td><td>{{row.Remark}}</td>
-                        <td><a href="#" @click="Edit(row)">编辑</a>&nbsp;&nbsp;<a href="#" @click="Delete(row.Id)">删除</a></td>
+                    <template v-for='(item,index) in tbs' >
+                        <tr>
+                            <td v-text='item.Name'></td>
+                            <td v-text='item.Age'></td>
+                            <td v-text='item.School'></td>
+                            <td v-text='item.Remark'></td>
+                            <td @click='edit(item)'>编辑</td>
+                            <td @click='del(item.Id)'>删除</td>
                         </tr>
                     </template>
                     <tr>
-                        <td><input type="text" class="form-control" v-model="rowtemplate.Name" /></td>
-                        <td><input type="text" class="form-control" v-model="rowtemplate.Age" /></td>
-                        <td><select class="form-control" v-model="rowtemplate.School">
-   　　　　　　　　　　　　　　　　 <option>中山小学</option>
-    　　　　　　　　　　　　　　　　<option>复兴中学</option>
-    　　　　　　　　　　　　　　　　<option>光明小学</option>
-　　　　　　　　　　　　　　　　</select></td>
-                        <td><input type="text" class="form-control" v-model="rowtemplate.Remark" /></td>
-                        <td><button type="button" class="btn btn-primary" v-on:click="Save">保存</button></td>
-                    </tr>
+                            <td> <input type='text'v-model='editTemplate.Name'></td>
+                            <td> <input type='text'v-model='editTemplate.Age'></td>
+                            <td> <select v-model='editTemplate.School'>
+                                <option>复兴中学</option>
+                                <option>中山小学</option>
+                                <option>光明小学</option>
+                            </select></td>
+                            <td> <input type='text'v-model='editTemplate.Remark'></td>
+                            <td> <input type='button' value='保存' @click='save'></td>
+                        </tr>
                 </tbody>
             </table>
         </div>
-    </div>
-	</div>
-</transition>
+   </transition>
 </template>
 <script>
-	export default{
-		data(){
-			return{
-				rows: [
-		            { Id: 1, Name: '小明', Age: 18, School: '光明小学', Remark: '三好学生' },
-		            { Id: 2, Name: '小刚', Age: 20, School: '复兴中学', Remark: '优秀班干部' },
-		            { Id: 3, Name: '吉姆格林', Age: 19, School: '光明小学', Remark: '吉姆做了汽车公司经理' },
-		            { Id: 4, Name: '李雷', Age: 25, School: '复兴中学', Remark: '不老实的家伙' },
-		            { Id: 5, Name: '韩梅梅', Age: 22, School: '光明小学', Remark: '在一起' },
-		            ],
-		            rowtemplate: { Id: 0, Name: '', Age: '', School: '', Remark: '' }
-					}
-			},
-		methods: {
-            Save: function (event) {
-                if (this.rowtemplate.Id == 0) {
-                    //设置当前新增行的Id
-                    this.rowtemplate.Id = this.rows.length + 1;
-                    this.rows.push(this.rowtemplate);
+    export default{
+        data(){
+            return{
+                editTemplate:{ "Id": '0', "Name": "", "Age": '', "School": "", "Remark": "" },
+                tbs:{
+                    type:Array
                 }
-                
-                //还原模板
-                this.rowtemplate = { Id: 0, Name: '', Age: '', School: '', Remark: '' }
+            }
+        },
+        mounted(){
+            this.$nextTick(function(){
+                this.fetchTabledata();
+            })
+        },
+        methods:{
+            fetchTabledata(){
+          
+                this.$http.get('static/data.json').then(res=>{
+                   this.tbs=res.data.table.tbs;
+                }).catch(err=>{
+                    console.log('tabledata',err)
+                })
             },
-            Delete: function (id) {
-                //实际项目中参数操作肯定会涉及到id去后台删除，这里只是展示，先这么处理。
-                for (var i=0;i<this.rows.length;i++){
-                    if (this.rows[i].Id == id) {
-                        this.rows.splice(i, 1);
-                        break;
+            edit(item){
+                this.editTemplate=item;
+            },
+            del(id){
+               //这里要用es6 将this作用域指向vue  否则作用域只是函数本身
+                this.tbs.forEach((item,index)=>{
+                    
+                    if(this.tbs[index].Id==id){
+                         this.tbs.splice(index,1);
                     }
-                }
+                })
             },
-            Edit: function (row) {
-                this.rowtemplate = row;
+            save(event){
+                if(this.editTemplate.Id==0){
+                    this.editTemplate.Id=this.tbs.length+1;
+                    this.tbs.push(this.editTemplate);
+                }
+                this.editTemplate={ "Id": '0', "Name": "", "Age": '', "School": "", "Remark": "" }
             }
         }
-	}
+    }
 </script>
-<style>
-	
-</style>
+<style></style> 
